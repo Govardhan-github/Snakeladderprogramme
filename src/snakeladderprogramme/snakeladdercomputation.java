28ba995dcef83edb4adcd3a31f9ef5cb4ad12265
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-07-13 22:02:18 -0700

    UC1 intial position added

--- src/snakeladderprogramme/snakeladdercomputation.java
+++ src/snakeladderprogramme/snakeladdercomputation.java
@@ -1,9 +1,11 @@
 package snakeladderprogramme;
 
 public class snakeladdercomputation {
+	//variables
+	static int position = 0;
 
 	public static void main(String[] args) {
-	System.out.println("Welcome to the snakeladder programme");
+	System.out.println("The player postion is " + position);
 
 	}
 
