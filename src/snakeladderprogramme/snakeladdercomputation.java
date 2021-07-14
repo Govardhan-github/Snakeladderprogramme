010121e1ed47cf1a013cd27c1064825199299ed8
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-07-14 05:34:23 -0700

    UC4 winning position added

--- src/snakeladderprogramme/snakeladdercomputation.java
+++ src/snakeladderprogramme/snakeladdercomputation.java
@@ -6,29 +6,40 @@ public class snakeladdercomputation {
 	int LADDER = 1;
 	int SNAKE = 2;
 	int NO_PLAY = 0;                                                              
-	double Reachedposition = 0,Droppedposition = 0;
-	int position = 0;
+	double position = 0;
+	int winposition = 100;
 	{
-	//for printing dice number
-	double dicenbr = Math.floor(((Math.random() * 10) % 6) +1);
-		System.out.println("The dice nbr is " + dicenbr);   
 		
-	//used randome function to get ladder , snake position
-	double option = Math.floor(Math.random() * 10) % 3;
-		System.out.println("The option is " + option);
-	
-	//if condtion for getting ladder and snake
-		if ( option == LADDER ) {
-			Reachedposition = +(dicenbr + position);
-			System.out.println("The player will move postions: " +Reachedposition);
-		}
-		else if ( option == SNAKE ) {
-			Droppedposition = (dicenbr - position);
-			System.out.println("The player will drop postions: " +Droppedposition);
+		//for loop for to print winning position
+		for( position = 0; position < winposition; position++ ) {    
+		
+			//used random function to get ladder , snake position
+			double option = Math.floor(Math.random() * 10) % 3;
+				System.out.println("The option is " + option);
+			//for printing dice number
+			double dicenbr = Math.floor(((Math.random() * 10) % 6) +1);
+				System.out.println("The dice nbr is " + dicenbr);
+			//if condition for getting ladder and snake
+				if ( option == LADDER ) {
+					position = (dicenbr + position);
+						System.out.println("The player will move postions: " +position);
+						}
+						
+				else if ( option == SNAKE ) {
+					position = (position - dicenbr);
+						System.out.println("The player will drop postions: " +position);
+									if( position == 0) {
+										System.out.println("Restart the game");
+									break;
+								}
+					  }
+				else
+						System.out.println("The player stays at same position");
 			}
-		else
-			System.out.println("The player stays at same position");
 	}
-}
+
+	}
+	
+	
 
 }
