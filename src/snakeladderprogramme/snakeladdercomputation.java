69e5ace73c7fd33e1b9a10565bf58b5cb7f58d0f
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-07-13 22:11:06 -0700

    UC2 dice rolled added

--- src/snakeladderprogramme/snakeladdercomputation.java
+++ src/snakeladderprogramme/snakeladdercomputation.java
@@ -6,6 +6,8 @@ public class snakeladdercomputation {
 
 	public static void main(String[] args) {
 	System.out.println("The player postion is " + position);
+	double dicenbr = Math.floor(((Math.random() * 10) % 6) +1);
+	System.out.println("The dice nbr is " + dicenbr);
 
 	}
 
