ceeba5f639c3089a7ce127ce4720d642a932de4f
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-07-13 21:54:27 -0700

    welcome message added

---
+++ src/snakeladderprogramme/snakeladdercomputation.java
@@ -0,0 +1,10 @@
+package snakeladderprogramme;
+
+public class snakeladdercomputation {
+
+	public static void main(String[] args) {
+	System.out.println("Welcome to the snakeladder programme");
+
+	}
+
+}
